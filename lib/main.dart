1d5db477fd1cd3f24619ffd54c86bef62868cfc2
WeitongSun <edwina@udel.edu>
2021-11-03 15:51:42 -0400

    login page with no page transfermation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:video_player/video_player.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'mapPage.dart';
 import 'carList.dart';
 
+FirebaseAuth auth = FirebaseAuth.instance;
 //**********Login and About PAGE***********
 void main() => runApp(BackgroundVideo());
 
@@ -12,20 +15,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FlutterApp',
-      debugShowCheckedModeBanner: true,
-      theme: ThemeData(
-        primaryColor: Colors.black12,
-        accentColor: Colors.orange,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Stateless'),
-          //backgroundColor: Colors.transparent,
-        ),
-      ),
-    );
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          return MaterialApp(
+            title: 'FlutterApp',
+            debugShowCheckedModeBanner: true,
+            theme: ThemeData(
+              primaryColor: Colors.black12,
+              accentColor: Colors.orange,
+            ),
+            home: Scaffold(
+              appBar: AppBar(
+                title: Text('Stateless'),
+                //backgroundColor: Colors.transparent,
+              ),
+            ),
+          );
+        });
   }
 }
 
@@ -100,12 +107,54 @@ class _BackgroundVideoState extends State<BackgroundVideo> {
   }
 }
 
+class LoginWidget extends StatefulWidget {
+  @override
+  _LoginWidget createState() => _LoginWidget();
+}
+
 // A basic login widget with a logo and a form with rounded corners.
 //test
-class LoginWidget extends StatelessWidget {
-  const LoginWidget({
-    Key key,
-  }) : super(key: key);
+class _LoginWidget extends State<LoginWidget> {
+  var _isLoginWindow;
+  var _username;
+  var _password;
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoginWindow = false;
+    _username = "";
+    _password = "";
+  }
+
+  void setUsername(context) {
+    setState(() {
+      _username = context;
+    });
+  }
+
+  void setPassword(context) {
+    setState(() {
+      _password = context;
+    });
+  }
+
+  void loginHandler() {
+    setState(() => _isLoginWindow = !_isLoginWindow);
+  }
+
+  Future<void> confirmLogin() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: _username, password: _password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -123,67 +172,115 @@ class LoginWidget extends StatelessWidget {
         SizedBox(
           height: 75.0,
         ),
-        Container(
-          decoration: new BoxDecoration(
-            color: Colors.white.withAlpha(200),
-            borderRadius: new BorderRadius.only(
-              topLeft: const Radius.circular(10.0),
-              topRight: const Radius.circular(10.0),
-              bottomLeft: const Radius.circular(10.0),
-              bottomRight: const Radius.circular(10.0),
-            ),
-          ),
-          padding: EdgeInsets.all(16),
-          width: 300,
-          height: 200,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              ButtonTheme(
-                minWidth: 300.0,
-                child: RaisedButton(
-                  color: Color(0xff00539f),
-                  child: Text(
-                    'Login as Guest',
-                    style: TextStyle(color: Color(0xffffd544), fontSize: 20),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => path()),
-                    );
-                  },
-                ),
-              ),
-              ButtonTheme(
-                minWidth: 300.0,
-                child: RaisedButton(
-                  color: Color(0xff00539f),
-                  child: Text(
-                    'Login as User',
-                    style: TextStyle(color: Color(0xffffd544), fontSize: 20),
-                  ),
-                  onPressed: () {},
-                ),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: const TextStyle(
-                    fontSize: 15,
-                    color: Color(0xff00539f),
+        _isLoginWindow
+            ? Container(
+                decoration: new BoxDecoration(
+                  color: Colors.white.withAlpha(200),
+                  borderRadius: new BorderRadius.only(
+                    topLeft: const Radius.circular(10.0),
+                    topRight: const Radius.circular(10.0),
+                    bottomLeft: const Radius.circular(10.0),
+                    bottomRight: const Radius.circular(10.0),
                   ),
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AboutUs()),
-                  );
-                },
-                child: const Text('About Us'),
+                padding: EdgeInsets.all(16),
+                width: 300,
+                height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Username',
+                      ),
+                      onSubmitted: (context) => {setUsername(context)},
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'Password'),
+                      onSubmitted: (context) => {setPassword(context)},
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(
+                          fontSize: 15,
+                          color: Color(0xff00539f),
+                        ),
+                      ),
+                      onPressed: () {
+                        confirmLogin();
+                      },
+                      child: const Text('confirm'),
+                    ),
+                  ],
+                ),
+              )
+            : Container(
+                decoration: new BoxDecoration(
+                  color: Colors.white.withAlpha(200),
+                  borderRadius: new BorderRadius.only(
+                    topLeft: const Radius.circular(10.0),
+                    topRight: const Radius.circular(10.0),
+                    bottomLeft: const Radius.circular(10.0),
+                    bottomRight: const Radius.circular(10.0),
+                  ),
+                ),
+                padding: EdgeInsets.all(16),
+                width: 300,
+                height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    ButtonTheme(
+                      minWidth: 300.0,
+                      child: RaisedButton(
+                        color: Color(0xff00539f),
+                        child: Text(
+                          'Login as Guest',
+                          style:
+                              TextStyle(color: Color(0xffffd544), fontSize: 20),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => path()),
+                          );
+                        },
+                      ),
+                    ),
+                    ButtonTheme(
+                      minWidth: 300.0,
+                      child: RaisedButton(
+                        color: Color(0xff00539f),
+                        child: Text(
+                          'Login as User',
+                          style:
+                              TextStyle(color: Color(0xffffd544), fontSize: 20),
+                        ),
+                        onPressed: () {
+                          loginHandler();
+                        },
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(
+                          fontSize: 15,
+                          color: Color(0xff00539f),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => AboutUs()),
+                        );
+                      },
+                      child: const Text('About Us'),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
       ],
     );
   }
